1965e2cdfe8f1fb817a44035c6dc6bf146473556
Thomas Wallingford <tbwallingford@gmail.com>
2022-12-05 20:25:40 -0700

    commit tree 7a1c6abddd5b2709ef7e97972968d27f56647e71

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(nameArray, celebration) {
+    debugger;
+    let newArray = [];
+    let i = 0;
+    while (i < nameArray.length) {
+        newArray.push("Thank you, " + nameArray[i] + ", for the wonderful " + celebration + " gift!");
+        debugger;
+        i = i + 1;
+    }
+    return newArray;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number = number - 1;
+    }
+}
